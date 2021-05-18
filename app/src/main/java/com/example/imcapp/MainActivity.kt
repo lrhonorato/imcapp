c22232cd401cf532dee5b192fa4aced12106c540
lrhonorato <lucas.rh@gmail.com>
2021-05-17 23:14:52 -0300

    Activitys tests

--- app/src/main/java/com/example/imcapp/MainActivity.kt
+++ app/src/main/java/com/example/imcapp/MainActivity.kt
@@ -1,43 +1,34 @@
 package com.example.imcapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.amin.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.w("Lifecycle", "CREATE - estou criando a tela")
+        setListeners()
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.w("Lifecycle", "START - deixei a tela visível para você")
+    private fun setListeners() {
+        alturaEDT?.doAfterTextChanged { text ->
+            // Toast.makeText(this, text.toString(), Toast.LENGTH_SHORT).show()
+        }
+        pesoEDT?.doOnTextChanged { text, __, __, _ ->
+            // titleTXT?.text = text
+        }
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.w("Lifecycle", "RESUME - agora você pode interagir com a tela")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.w("Lifecycle", "PAUSE - a tela perdeu o foco, você não pode mais interagir")
-    }
-
-    override fun onStop() {
-        Log.w("Lifecycle", "STOP -  a tela não está mais visível mas ainda existe")
-        super.onStop()
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.w("Lifecycle", " \nRESTART - a tela está retomando o foco")
-    }
-
-    override fun onDestroy() {
-        Log.w("Lifecycle", " \nDESTROY - oh não! a tela foi destruída")
-        super.onDestroy()
+    private fun calcularIMC(peso: String, altura: String) {
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+        if (peso != null && altura != null) {
+            val imc = peso / (altura * altura)
+            titleTXT.text = "Seu IMC é de \$".format(imc)
+        }
     }
 }
